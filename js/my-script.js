8ae98c0a93086bdef875074b0cc1309e1f8495a7
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-29 15:44:59 +0200

    funzioni aggiungi e elimina aggiunti

--- js/my-script.js
+++ js/my-script.js
@@ -5,12 +5,15 @@ new Vue (
         el: '#app', 
         data:{
             list_task: [],
-            add_Task = ''
+            add_Task: ''
         },
         methods:{
             pushTask: function(){
                 this.list_task.push(this.add_Task);
                 this.add_Task = ''
+            },
+            deleteTask: function(actual_position){
+                this.list_task.splice(actual_position , 1)
             }
         }
     }
